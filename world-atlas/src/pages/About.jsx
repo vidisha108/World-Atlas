fd1ca828e1a902f721a84b8aa7c05ff340da3f63
Vidisha Srivastava <vidishasrivastava2001@gmail.com>
2025-07-09 19:05:52 +0530

    adding json data into cards

--- world-atlas/src/pages/About.jsx
+++ world-atlas/src/pages/About.jsx
@@ -1,3 +1,45 @@
+import countryFacts from "../api/countryData.json"
 export const About = () =>{
-    return <h1>About Page</h1>
+    return (
+        <section className="section-about container">
+            <h2 className="container-title">
+                Here are the Interesting Facts
+                <br />
+                we're proud of 
+            </h2>
+            <div className="gradient-cards">
+
+                {
+                    countryFacts.map((country) => {
+
+                        return(
+
+                        <div className="card" key={country.id}>
+                        <div className="container-card bg-blue-box">
+                        <p className="card-title">{country.countryName}</p>
+                        <p>
+                            <span className="card-description">Capital:</span>
+                            {country.capital}
+                        </p>
+                        <p>
+                            <span className="card-description">Population:</span>
+                            {country.population}
+                        </p>
+                        <p>
+                            <span className="card-description">Facts:</span>
+                            {country.interestingFact}
+                        </p>
+                    </div>
+
+                </div>
+
+                        )
+                    })
+                }
+
+                
+
+            </div>
+        </section>
+    )
 };
