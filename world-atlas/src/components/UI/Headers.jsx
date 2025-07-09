63665518b1d2694a16adadb899e24b01dc1a9553
Vidisha Srivastava <vidishasrivastava2001@gmail.com>
2025-07-09 10:26:41 +0530

    creating navbar

--- world-atlas/src/components/UI/Headers.jsx
+++ world-atlas/src/components/UI/Headers.jsx
@@ -1,3 +1,34 @@
+import { NavLink } from "react-router-dom"
+
 export const Headers = ()=>{
-    return <h1>head</h1>
+    return (
+    <header>
+        <div className="container">
+            <div className="grid navbar-grid">
+                <div className="logo">
+                    <NavLink to="/">
+                        <h1>WorldAtlas</h1>
+                    </NavLink>
+                </div>
+                <nav>
+                    <ul>
+                        <li>
+                            <NavLink to="/">Home</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/about">About</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/Country">Country</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/contact">Contact</NavLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+    </header>
+    )
 }
--- world-atlas/src/pages/ErrorPage.jsx
+++ world-atlas/src/pages/ErrorPage.jsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom"
+import { NavLink, useRouteError } from "react-router-dom"
 
 export const ErrorPage = () =>{
     const error = useRouteError();
@@ -7,6 +7,11 @@ export const ErrorPage = () =>{
     <div>
     <h1>Oops! An error occurred</h1>
     {error && <p>{error.data}</p>}
+    <NavLink to="/"> 
+    <button>
+    Go Home
+    </button>
+    </NavLink>
     </div>
     )
 }
